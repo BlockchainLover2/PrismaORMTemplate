a93ff88a838d3c01b7f8ed83c5d7144302224fed
BlockchainLover2 <batuyek442@gmail.com>
2024-08-16 21:01:27 +0300

    Updated

--- db/queries.js
+++ db/queries.js
@@ -1,20 +1,22 @@
-const pool = require('./pool')
+// instantiate the client
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 
-async function getUserById(id){
-    return (await pool.query('SELECT * FROM users WHERE id = $1', [id])).rows[0]
+async function createUser(){
+    await prisma.users.create({
+        data: {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@doe.com',
+            password: 'password',
+        }
+    })
+    await prisma.$disconnect()
 }
-async function getUserByUsername(username){
-    return (await pool.query('SELECT * FROM users WHERE username = $1', [username])).rows[0]
-}
-
-async function createUser(username,password){
-    await pool.query('INSERT INTO users (username,password) VALUES ($1,$2)', [username, password])
-}
 
 
-module.exports = {
-    getUserById,
-    getUserByUsername,
-    createUser
-}
+
+
+// when fetching all messages
+const messages = await prisma.users.findMany();
